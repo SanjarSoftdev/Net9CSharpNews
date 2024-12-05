a13d41329493037413c6a9757f2f8c28a3958ee2
Sanjar <sanjar.softdev@gmail.com>
2024-12-05 17:01:30 +0500

    some changes

--- CSharpNews/CSharpNews/ParamsExpansionDemo.cs
+++ CSharpNews/CSharpNews/ParamsExpansionDemo.cs
@@ -1,17 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-
-internal class ParamsExpansionDemo
+﻿internal class ParamsExpansionDemo
 {
-    [OverloadResolutionPriority(1)]
     public static void PrintNumbersCount(params int[] numbers)
         => Console.WriteLine(numbers.Length);
 
     public static void PrintNumbersCount(params IEnumerable<int> numbers)
         => Console.WriteLine(numbers.Count());
 
-    [OverloadResolutionPriority(2)]
-    public static void PrintNumbersCount(params List<int> numbers)
-        => Console.WriteLine(numbers.Count());
+    public static void PrintItemsCount<T>(params ReadOnlySpan<T> items)
+        => Console.WriteLine(items.Length);
 
     public static void PrintItemsCount<T>(params T[] items)
         => Console.WriteLine(items.Length);
@@ -20,8 +16,6 @@ internal class ParamsExpansionDemo
     {
         PrintNumbersCount(new int[] { 1, 2, 3, 4, 5 });
 
-        PrintNumbersCount(1, 2, 3, 4, 5);
-
-        PrintItemsCount(["apple", "banana", "cherry"]);
+        PrintNumbersCount(new int[] { 11, 12, 13 });
     }
 }
